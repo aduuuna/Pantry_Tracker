17d31e406a99eff46504c4ad9199d23fbc222a98
Owusu Ansah Joy <owusujoyansah@gmail.com>
2024-07-31 20:21:19 +0000

    ##

--- inventory-management/firebase.js
+++ inventory-management/firebase.js
@@ -1,13 +1,7 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
 import { getFirestore } from "firebase/firestore";
 
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
 // Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyB1tmoQa9Ow_EIjPGmB29lvetQlMvAdtnE",
   authDomain: "inventory-management-26f5c.firebaseapp.com",
@@ -20,7 +14,13 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
 const firestore = getFirestore(app);
 
-export { firestore };
+// Initialize Analytics only on the client side
+let analytics = null;
+if (typeof window !== "undefined") {
+  const { getAnalytics } = require("firebase/analytics");
+  analytics = getAnalytics(app);
+}
+
+export { firestore, analytics };
